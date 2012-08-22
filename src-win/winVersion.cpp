5395284455e466a392ccffbbaf5f0896b1e1f9e3
John <John@Aluminium.localdomain>
2012-08-22 10:01:42 -0700

    winCLI - update version to 3.0.1(v30)

--- src-win/winVersion.cpp
+++ src-win/winVersion.cpp
@@ -39,11 +39,11 @@ const char* APP_NAME(const char* argv0)
 
 const char* APP_VERSION()
 { 
-    return "2.2"; 
+    return "3.0.1"; 
 }
 int APP_BUILD()
 { 
-    return 9; 
+    return 30; 
 }
 char APP_OPTCHAR()
 { 
