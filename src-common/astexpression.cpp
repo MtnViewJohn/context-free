b24a7e2417b1db40ea1abef516e85241a3cb4794
John W. Horigan <john@glyphic.com>
2012-09-05 07:52:58 -0700

    common - fix ASTuserFunc::simplify() to never collapse the outer ASTcons in the arguments

--- src-common/astexpression.cpp
+++ src-common/astexpression.cpp
@@ -2232,8 +2232,15 @@ namespace AST {
     ASTexpression*
     ASTuserFunction::simplify()
     {
-        if (arguments)
-            arguments = arguments->simplify();
+        if (arguments) {
+            ASTcons* carg = dynamic_cast<ASTcons*>(arguments);
+            if (carg) {
+                for (size_t i = 0; i < carg->children.size(); ++i)
+                    carg->children[i] = carg->children[i]->simplify();
+            } else {
+                arguments = arguments->simplify();
+            }
+        }
         return this;
     }
     
