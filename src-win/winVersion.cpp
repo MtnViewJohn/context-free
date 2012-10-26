85216422f5f4627a9023c3f031b92e3026d82b26
John <John@Aluminium.localdomain>
2012-10-25 22:06:07 -0700

    winCLI - bumped version to 3.0.3(32)

--- src-win/winVersion.cpp
+++ src-win/winVersion.cpp
@@ -39,11 +39,11 @@ const char* APP_NAME(const char* argv0)
 
 const char* APP_VERSION()
 { 
-    return "3.0.2"; 
+    return "3.0.3"; 
 }
 int APP_BUILD()
 { 
-    return 31; 
+    return 32; 
 }
 char APP_OPTCHAR()
 { 
