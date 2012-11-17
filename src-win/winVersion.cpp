ae0451f45dcafd1c50c57acdc8bf8aaea99edce9
John <John@no-h8.localdomain>
2012-11-16 23:15:25 -0800

    winCLI - update version to 3.0.4(33)

--- src-win/winVersion.cpp
+++ src-win/winVersion.cpp
@@ -39,11 +39,11 @@ const char* APP_NAME(const char* argv0)
 
 const char* APP_VERSION()
 { 
-    return "3.0.3"; 
+    return "3.0.4"; 
 }
 int APP_BUILD()
 { 
-    return 32; 
+    return 33; 
 }
 char APP_OPTCHAR()
 { 
