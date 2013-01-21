188dc10a4bae3d9db6ad6e7557b7f1584a22150b
John W. Horigan <john@glyphic.com>
2013-01-21 12:48:42 -0800

    common - update copyright notices to 2013

--- src-common/ast.h
+++ src-common/ast.h
@@ -1,7 +1,7 @@
 // ast.h
 // this file is part of Context Free
 // ---------------------
-// Copyright (C) 2009-2012 John Horigan - john@glyphic.com
+// Copyright (C) 2009-2013 John Horigan - john@glyphic.com
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
