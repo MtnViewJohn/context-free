030535c9c3358d94ee6e80bf1bca1dd5bfb676e0
John W. Horigan <john@glyphic.com>
2013-01-21 12:48:42 -0800

    common - update copyright notices to 2013

--- src-common/stacktype.h
+++ src-common/stacktype.h
@@ -1,7 +1,7 @@
 // stacktype.h
 // this file is part of Context Free
 // ---------------------
-// Copyright (C) 2011 John Horigan - john@glyphic.com
+// Copyright (C) 2011-2013 John Horigan - john@glyphic.com
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
