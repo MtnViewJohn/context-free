27bfcb5e7028e453834116d745a5990956a49cf1
John <John@no-h8.localdomain>
2013-03-30 14:38:03 -0700

    winCLI - bump to 3.0.6(35)

--- src-win/winVersion.cpp
+++ src-win/winVersion.cpp
@@ -39,11 +39,11 @@ const char* APP_NAME(const char* argv0)
 
 const char* APP_VERSION()
 { 
-    return "3.0.5"; 
+    return "3.0.6"; 
 }
 int APP_BUILD()
 { 
-    return 34; 
+    return 35; 
 }
 char APP_OPTCHAR()
 { 
