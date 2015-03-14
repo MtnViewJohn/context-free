2ed8e0f64a02f8412e1b746307364d099c92ffff
John W. Horigan <john@glyphic.com>
2015-03-14 15:57:36 -0700

    common - add regular input files to tests

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for file in input/tests/*.cfdg ; do echo -n "$file "; if ./cfdg -Pq "$file" output/test.png ; then echo '   pass' ; : ; else echo '          FAIL' ; break; fi ; done
+for file in input/tests/*.cfdg input/*.cfdg ; do echo -n "$file "; if ./cfdg -Pq "$file" output/test.png ; then echo '   pass' ; : ; else echo '          FAIL' ; break; fi ; done
