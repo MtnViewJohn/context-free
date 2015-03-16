3136c788d1287c115de9081f4869ea7c499fd8cf
John Horigan <john@glyphic.com>
2015-03-15 22:34:04 -0700

    posix - test script should create output directory

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+mkdir output
 for file in input/tests/*.cfdg input/*.cfdg ; do echo -n "$file "; if ./cfdg -Pq "$file" output/test.png ; then echo '   pass' ; : ; else echo '          FAIL' ; break; fi ; done
