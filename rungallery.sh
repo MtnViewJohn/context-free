601f6980e0d044d2ee2be1dfcbb790657d464a07
John W. Horigan <john@glyphic.com>
2015-03-24 23:00:15 -0700

    all - add rungallery.sh script for testing against gallery cfdg files

---
+++ rungallery.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ $# == 0 ]]; then
+	echo "usage: rungallery.sh list_of_batches";
+	echo "  Where a batch is either a gallery id or a range id-id" 
+	exit 0;
+fi
+
+[ -d output ] || mkdir output
+
+while [[ $# > 0 ]]; do
+	if [[ $1 =~ ([0-9]+)-([0-9]+) ]]; then
+		echo "From ${BASH_REMATCH[1]} to ${BASH_REMATCH[2]}"
+		for (( id = ${BASH_REMATCH[1]}; id <= ${BASH_REMATCH[2]}; ++id )); do
+			echo -n "$id";
+			curl -GLs "http://www.contextfreeart.org/gallery/data.php?id=$id&type=cfdg" | ./cfdg -qP - output/test.png
+			if [[ $? == 0 ]]; then
+				echo '   pass';
+			else
+				echo '          FAIL';
+			fi
+		done
+	else
+		echo -n "$1";
+		curl -GLs "http://www.contextfreeart.org/gallery/data.php?id=$1&type=cfdg" | ./cfdg -qP - output/test.png
+		if [[ $? == 0 ]]; then
+			echo '   pass';
+		else
+			echo '          FAIL';
+		fi
+	fi;
+	shift;
+done
