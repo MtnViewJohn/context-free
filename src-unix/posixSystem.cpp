9507ebffd7341b147cdb19b2b3d43e35561fa43b
John W. Horigan <john@glyphic.com>
2015-05-04 10:39:46 -0700

    posix/osx - use mkstemp() instead of mktemp() for creating temp files, it's safer

--- src-unix/posixSystem.cpp
+++ src-unix/posixSystem.cpp
@@ -42,6 +42,90 @@
 #include <cstdint>
 using namespace std;
 
+/* The following code declares classes to read from and write to
+ * file descriptore or file handles.
+ *
+ * See
+ *      http://www.josuttis.com/cppcode
+ * for details and the latest version.
+ *
+ * - open:
+ *      - integrating BUFSIZ on some systems?
+ *      - optimized reading of multiple characters
+ *      - stream for reading AND writing
+ *      - i18n
+ *
+ * (C) Copyright Nicolai M. Josuttis 2001.
+ * Permission to copy, use, modify, sell and distribute this software
+ * is granted provided this copyright notice appears in all copies.
+ * This software is provided "as is" without express or implied
+ * warranty, and with no claim as to its suitability for any purpose.
+ *
+ * Version: Jul 28, 2002
+ * History:
+ *  Jul 28, 2002: bugfix memcpy() => memmove()
+ *                fdinbuf::underflow(): cast for return statements
+ *  Aug 05, 2001: first public version
+ */
+#include <ostream>
+#include <streambuf>
+// for EOF:
+#include <cstdio>
+
+
+// low-level read and write functions
+#include <unistd.h>
+
+
+// BEGIN namespace BOOST
+namespace boost {
+    
+    
+    /************************************************************
+     * fdostream
+     * - a stream that writes on a file descriptor
+     ************************************************************/
+    
+    
+    class fdoutbuf : public std::streambuf {
+    protected:
+        int fd;    // file descriptor
+    public:
+        // constructor
+        fdoutbuf (int _fd) : fd(_fd) {
+        }
+    protected:
+        // write one character
+        virtual int_type overflow (int_type c) {
+            if (c != EOF) {
+                char z = c;
+                if (write (fd, &z, 1) != 1) {
+                    return EOF;
+                }
+            }
+            return c;
+        }
+        // write multiple characters
+        virtual
+        std::streamsize xsputn (const char* s,
+                                std::streamsize num) {
+            return write(fd,s,num);
+        }
+    };
+    
+    class fdostream : public std::ostream {
+    protected:
+        fdoutbuf buf;
+    public:
+        fdostream (int fd) : std::ostream(0), buf(fd) {
+            rdbuf(&buf);
+        }
+    };
+
+}
+
+// End of Nicolai M. Josuttis code
+
 void
 PosixSystem::clearAndCR()
 {
@@ -75,12 +159,12 @@ PosixSystem::tempFileForWrite(AbstractSystem::TempType tt, string& nameOut)
     t.append(TempPrefixes[tt]);
     t.append("XXXXXX");
     
-    ofstream* f = nullptr;
+    ostream* f = nullptr;
     
     unique_ptr<char, decltype(&free)> b(strdup(t.c_str()), &free);
-    if (mktemp(b.get())) {
-        f = new ofstream;
-        f->open(b.get(), ios::binary | ios::trunc | ios::out);
+    int tfd = mkstemp(b.get());
+    if (tfd != -1) {
+        f = new boost::fdostream(tfd);
         nameOut.assign(b.get());
     }
     
