97da27b8a580eace2c5ce23d492ab14279511716
John W. Horigan <john@glyphic.com>
2016-02-17 15:19:10 -0800

    posix - bump version to 3.0.10(26)

--- src-unix/posixVersion.cpp
+++ src-unix/posixVersion.cpp
@@ -39,11 +39,11 @@ const char* APP_NAME(const char* argv0)
 
 const char* APP_VERSION()
 { 
-    return "3.0.9";
+    return "3.0.10";
 }
 int APP_BUILD()
 { 
-    return 25;
+    return 26;
 }
 char APP_OPTCHAR()
 { 
