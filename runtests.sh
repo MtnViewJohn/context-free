cad9472600e6e163dfcab061bbd1fc8364a62a5a
John W. Horigan <john@glyphic.com>
2016-08-31 08:45:17 -0700

    cli - rewrite to not use echo -n and remove bash dependence

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 mkdir output
-for file in input/tests/*.cfdg input/*.cfdg ; do echo -n "$file "; if ./cfdg -Pq "$file" output/test.png ; then echo '   pass' ; : ; else echo '          FAIL' ; break; fi ; done
+for file in input/tests/*.cfdg input/*.cfdg ; do if ./cfdg -Pq "$file" output/test.png ; then echo "$file   pass" ; : ; else echo "$file          FAIL" ; break; fi ; done
