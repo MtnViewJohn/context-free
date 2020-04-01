c6fdf97d05f80ad7ba95261d69d1fed02b4e51dd
John W. Horigan <john@glyphic.com>
2020-03-31 19:29:07 -0700

    common - parse design JSON from Gallery I instead of Gallery II

--- src-common/upload.cpp
+++ src-common/upload.cpp
@@ -155,9 +155,11 @@ Upload::Upload(const char* jsonbuf, std::size_t jsonlen)
     // is kind of janky.
     try {
         auto j = json::parse({jsonbuf, jsonlen});
-        mId = j.at("design").at("designid").get<int>();
+        mId = j.at("design").at("id").get<int>();
         mFileName = j.at("design").at("filelocation").get<std::string>();
-        mVariation = Variation::fromString(j.at("design").at("variation").get<std::string>().c_str());
+        try {
+            mVariation = Variation::fromString(j.at("design").at("variation").get<std::string>().c_str());
+        } catch (...) {}
         try {
             // Only present for gallery download
             mPassword = j.at("cfdg").get<std::string>();
