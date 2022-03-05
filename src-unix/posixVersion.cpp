1848bfb57bd437bd8453f7efcc56333b21586ad1
John Horigan <john@glyphic.com>
2022-03-04 22:23:04 -0800

    posix - bump version to 3.3.2(35)

--- src-unix/posixVersion.cpp
+++ src-unix/posixVersion.cpp
@@ -39,11 +39,11 @@ const char* APP_NAME(const char* argv0)
 
 const char* APP_VERSION()
 { 
-    return "3.3.1";
+    return "3.3.2";
 }
 int APP_BUILD()
 { 
-    return 34;
+    return 35;
 }
 const char* APP_OPTCHAR()
 { 
