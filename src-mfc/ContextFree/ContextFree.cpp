c333352dad10f06aa098c814512a655fc7feff69
John Horigan <john@glyphic.com>
2025-11-23 21:20:31 -0700

    win - update from 3.4.2(49) to 3.4.2(50)

--- src-mfc/ContextFree/ContextFree.cpp
+++ src-mfc/ContextFree/ContextFree.cpp
@@ -277,7 +277,7 @@ L"This program is distributed in the hope that it will be useful, but WITHOUT AN
 L"You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 - 1307 USA"
 )
 , m_sMainCopyright(_T("Copyright © 2005-2025\r\nJohn Horigan && Mark Lentczner"))
-, m_sTitle(_T("Context Free, Version 3.4.2(v49)"))
+, m_sTitle(_T("Context Free, Version 3.4.2(v50)"))
 {
 }
 
