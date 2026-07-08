72f84cdfb391f7c00598285eac48de5119b7348c
John W. Horigan <john@glyphic.com>
2026-07-08 15:17:31 -0700

    common - change ffCanvasDummy to match change in ffCanvas

--- src-common/ffCanvasDummy.cpp
+++ src-common/ffCanvasDummy.cpp
@@ -51,7 +51,7 @@ public:
 };
 
 
-ffCanvas::ffCanvas(const char*, PixelFormat fmt, int width, int height, int, QTcodec, bool)
+ffCanvas::ffCanvas(const char*, PixelFormat fmt, int width, int height, int, QTcodec, bool, int)
 : aggCanvas(mapPixFmt(fmt)), mErrorMsg("Quicktime support not compiled in")
 {
     width &= ~3;
